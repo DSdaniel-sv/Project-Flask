c3359927ab5fad81d4b37383b8ee45e1756c2a44
daniel <danielsilvavalencia25@gmail.com>
2023-04-06 19:11:35 -0500

    cambios .env y init(app)

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from config import environment
 
 
 app = Flask(__name__)
+app.config.from_object(environment[getenv('ENVIRONMENT')])
+
 api = Api(
     app, 
     title='BoilerPlate1',
     description='EndPoinst BoilerPlate',
     doc='/swagger-ui'
 )
+
+db = SQLAlchemy(app)
